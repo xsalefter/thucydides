429987330fde14a92ae44da4b530a831a6f52132
xsalefter <xsalefter@gmail.com>
2013-02-17 17:13:00 +0700

    Add Internationalization initialization in constructor. Additionally, #usingContext method will add context with internationalization data.

--- thucydides-core/src/main/java/net/thucydides/core/reports/html/HtmlReporter.java
+++ thucydides-core/src/main/java/net/thucydides/core/reports/html/HtmlReporter.java
@@ -2,6 +2,7 @@ package net.thucydides.core.reports.html;
 
 import net.thucydides.core.ThucydidesSystemProperty;
 import net.thucydides.core.guice.Injectors;
+import net.thucydides.core.reports.Internationalization;
 import net.thucydides.core.reports.templates.ReportTemplate;
 import net.thucydides.core.reports.templates.TemplateManager;
 import net.thucydides.core.util.EnvironmentVariables;
@@ -12,6 +13,7 @@ import org.slf4j.LoggerFactory;
 import java.io.File;
 import java.io.IOException;
 import java.io.StringWriter;
+import java.util.Locale;
 import java.util.Map;
 
 /**
@@ -27,16 +29,25 @@ public abstract class HtmlReporter {
     private final TemplateManager templateManager;
     private final EnvironmentVariables environmentVariables;
 
+    private Internationalization internationalization;
+
     private static final Logger LOGGER = LoggerFactory.getLogger(HtmlReporter.class);
 
     public HtmlReporter() {
         this(Injectors.getInjector().getInstance(EnvironmentVariables.class));
+        this.internationalization = new Internationalization();
     }
 
     public HtmlReporter(final EnvironmentVariables environmentVariables) {
         super();
         this.templateManager = Injectors.getInjector().getInstance(TemplateManager.class);
         this.environmentVariables = environmentVariables;
+        String language = environmentVariables.getProperty(ThucydidesSystemProperty.TEST_REPORT_LANGUAGE);
+        if (language.isEmpty()) {
+        	this.internationalization = new Internationalization();
+        } else {
+        	this.internationalization = new Internationalization(new Locale(language));
+        }
     }
 
     private TemplateManager getTemplateManager() {
@@ -114,6 +125,7 @@ public abstract class HtmlReporter {
 
         public String usingContext(final Map<String, Object> context) {
             try {
+            	context.putAll(internationalization.getBundlesAsMap());
                 ReportTemplate template = getTemplateManager().getTemplateFrom(templateFile);
                 StringWriter sw = new StringWriter();
                 template.merge(context, sw);
